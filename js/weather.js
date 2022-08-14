2c8567098254e8bc22f2f530d5550929e35970ad
Sojin Jang <111125577+sojinjang@users.noreply.github.com>
2022-08-15 05:41:37 +0900

    feat: clock, todo, greeting, quotes, weather, background

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "b8a65de4c45dc8f5ec1555a9ea418113"
+
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude
+    const lon = position.coords.longitude
+    console.log("You live in ", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
